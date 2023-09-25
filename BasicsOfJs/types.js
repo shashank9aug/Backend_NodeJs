ae4b538267a44dfe3b2c0730416401b50e96c963
Shashank Shekhar <shashanksdev@gmail.com>
2023-09-26 01:41:15 +0530

    Data types in Js.

---
+++ BasicsOfJs/types.js
@@ -0,0 +1,29 @@
+console.log(12);
+console.log(-21);
+console.log(12.22);
+console.log(-0);
+
+console.log(typeof(12))
+
+console.log("shashank");
+console.log(typeof("shashank"));
+console.log(`this is backtick string`);
+
+console.log(false);
+console.log(typeof(false));
+
+//show type of emptiness
+console.log(null);
+console.log(typeof(null)); //object 
+
+console.log(undefined);
+console.log(typeof(undefined));
+
+//object: store key:value pair
+console.log({"key": 12});
+
+//symbol: immutable data type.
+console.log(Symbol('hello'))
+
+
+console.log(typeof(NaN))
