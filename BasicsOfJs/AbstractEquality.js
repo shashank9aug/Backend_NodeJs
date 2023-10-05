b4f9f42e1cdf0a3843422ea3b6f0d02d9b6a4902
Shashank Shekhar <shekhar1245shashank@gmail.com>
2023-10-05 06:13:56 +0530

    4th October learning[Abstract and Strict Equality]

---
+++ BasicsOfJs/AbstractEquality.js
@@ -0,0 +1,13 @@
+console.log()
+
+/**
+ * if x = null and y = undefined 
+ *  return true.
+ * 
+ * if x = undefined and y = null 
+ *  return true.
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ BasicsOfJs/strictEquality.js
@@ -0,0 +1,23 @@
+// console.log(NaN==NaN)
+console.log(NaN===NaN)
+//false
+
+console.log(33 === 33)
+console.log(undefined === undefined)
+console.log(null === null)
+
+// true
+// true
+// true
+
+console.log({}==={});
+//false
+
+let x = {};
+console.log(x===x);
+// true
+
+
+
+
+
